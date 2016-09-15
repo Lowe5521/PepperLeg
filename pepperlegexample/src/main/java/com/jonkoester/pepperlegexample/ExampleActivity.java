8e5a46ea240552e3dc815821ced4a22c921b2a07
Jonathan Koester <Jonathan.D.Koester@ehi.com>
2016-09-15 10:00:04 -0500

    - Added some example code for the Pepperleg library

--- pepperlegexample/src/main/java/com/jonkoester/pepperlegexample/ExampleActivity.java
+++ pepperlegexample/src/main/java/com/jonkoester/pepperlegexample/ExampleActivity.java
@@ -2,12 +2,38 @@ package com.jonkoester.pepperlegexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.jonkoester.pepperleg.PepperLegDataModel;
+import com.jonkoester.pepperleg.PepperLegOverlay;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class ExampleActivity extends AppCompatActivity {
+
+    public Queue<PepperLegDataModel> pepperQueue = new LinkedList<>();
+    public PepperLegOverlay pepperLegOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
+
+        getFragmentManager().beginTransaction()
+                .add(R.id.act_example_container1, new FragmentTop(), FragmentTop.class.getSimpleName())
+                .add(R.id.act_example_container2, new FragmentMid(), FragmentMid.class.getSimpleName())
+                .add(R.id.act_example_container3, new FragmentBot(), FragmentBot.class.getSimpleName())
+                .commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (pepperLegOverlay == null) {
+            pepperLegOverlay = new PepperLegOverlay(this, pepperQueue);
+            addContentView(pepperLegOverlay, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
     }
 }
---
+++ pepperlegexample/src/main/java/com/jonkoester/pepperlegexample/FragmentTop.java
@@ -0,0 +1,25 @@
+package com.jonkoester.pepperlegexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jonkoester.pepperleg.PepperLegDataModel;
+
+public class FragmentTop extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_top, container, false);
+
+        ((ExampleActivity) getActivity()).pepperQueue.add(new PepperLegDataModel(view.findViewById(R.id.first_name_tv), "First Name", "Is this thing on?", PepperLegDataModel.Direction.RIGHT, null, PepperLegDataModel.YAlignment.TOP));
+        ((ExampleActivity) getActivity()).pepperQueue.add(new PepperLegDataModel(view.findViewById(R.id.button2), "Button Two", "Button 2 is actually the third button... weird", PepperLegDataModel.Direction.BOTTOM, PepperLegDataModel.XAlignment.LEFT, null));
+        ((ExampleActivity) getActivity()).pepperQueue.add(new PepperLegDataModel(view.findViewById(R.id.tab_five_tv), "Table Middle!", "Dead center, baby!", PepperLegDataModel.Direction.TOP, PepperLegDataModel.XAlignment.CENTER, null));
+
+        return view;
+    }
+}
